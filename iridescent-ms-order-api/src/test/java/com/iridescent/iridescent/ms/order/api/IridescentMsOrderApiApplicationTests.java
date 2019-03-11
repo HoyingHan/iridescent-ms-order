ddf33ecc9062cd3a25530f2d3af7d8a540ea4f99
Sojournerhhy <han_hongying@163.com>
2019-03-11 15:55:41 +0800

    @dev 多模块改造

---
+++ iridescent-ms-order-api/src/test/java/com/iridescent/iridescent/ms/order/api/IridescentMsOrderApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iridescent.iridescent.ms.order.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IridescentMsOrderApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ iridescent-ms-order-service/src/main/java/com/iridescent/iridescent/ms/order/service/IridescentMsOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.iridescent.iridescent.ms.order.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IridescentMsOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IridescentMsOrderServiceApplication.class, args);
+    }
+
+}
